c550d2882be92b6e03eab3186fb40168068e53b1
William Sankey <william.sankey@gmail.com>
2014-06-24 11:36:30 -0400

    Setup project
    Working to establish classes in the company.py file with logic and controlls in list_of_companies.py file.

---
+++ List_of_companies.py
@@ -0,0 +1,31 @@
+import companies
+
+Almar = companies.Subcontractor(project_manager = "Mike Capala", trade = "plumbing")
+
+print Almar.project_manager
+
+Almar = companies.Company(project_manager = "Mike Capala", trade = "plumbing", company_description = "plumbing / heating", 
+company_address_street = "680 Madison Ave",
+company_address_city = "New York", 
+company_address_suite = "", 
+company_address_zip = "11238",  
+company_address_state = "New York",
+company_url = "https://www.facebook.com/pages/Almar-Plumbing-Heating-Corp/143200015725831",
+company_logo = "http://media.merchantcircle.com/16422355/Logo8-24_full.jpeg", 
+billing_type = "Requisition", 
+retainage_percent = "ten",
+)
+
+
+#
+#print Almar.description
+#print Almar.street
+#print Almar.city
+#print Almar.suite
+#print Almar.zip
+#print Almar.state
+#print Almar.url
+#print Almar.logo
+
+
+companies.Company.show_website(Almar)
---
+++ companies.py
@@ -0,0 +1,42 @@
+
+import webbrowser
+
+class Company():
+	def __init__(self, company_description, company_address_street, company_address_city, company_address_suite, company_address_zip, company_address_state, company_url, company_logo, billing_type, retainage_percent):
+		self.description = company_description
+		self.street = company_address_street
+		self.city = company_address_city
+		self.suite = company_address_suite
+		self.zip = company_address_zip
+		self.state = company_address_state
+		self.url = company_url
+		self.logo = company_logo
+		self.billing_type = billing_type
+		self.retainage_percent = retainage_percent
+		
+	def show_website(self):
+		webbrowser.open(self.url)
+		
+
+class Subcontractor(Company):
+	def __init__(self, project_manager, trade):
+		Company.__init__(self, company_description, company_address_street, company_address_city, company_address_suite, company_address_zip, company_address_state, company_url, company_logo, billing_type, retainage_percent)
+		self.project_manager = project_manager
+		self.trade = trade
+
+
+class Material_Supplier(Company):
+	def __init__(self, material_type, deposit_required, primary_contact):
+		Company.__init__(self, company_description, company_address_street, company_address_city, company_address_suite, company_address_zip, company_address_state, company_url, company_logo, billing_type, retainage_percent)
+		self.material_type = material_type
+		self.deposit_required = deposit_required
+		self.primary_contact = primary_contact
+		
+class Consultant(Company):
+	def __init__(self, professional_service_type, primary_contact, project_billing_phase):
+		Company.__init__(self, company_description, company_address_street, company_address_city, company_address_suite, company_address_zip, company_address_state, company_url, company_logo, billing_type, retainage_percent)
+		self.professional_service_type = professional_service_type
+		self.primary_contact = primary_contact
+		self.project_billing_phase = project_billing_phase
+	
+	
